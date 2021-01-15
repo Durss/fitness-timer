46e790c0c502c6f514c06737ca05ce0819cf4b44
Francois <durss@free.fr>
2021-01-15 17:04:22 +0100

    Cleaning uselss stuff. Fixing less init.

--- server/bootstrap.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const HTTPServer_1 = require("./server/HTTPServer");
-const Config_1 = require("./utils/Config");
-new HTTPServer_1.default(Config_1.default.SERVER_PORT);
